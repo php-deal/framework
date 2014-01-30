e45ee9268d5375d03ee850247f50d1c24c4cd2dc
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-01-30 19:11:09 +0400

    Add default implementation of AspectKernel with pre-configured aspect

---
+++ src/PhpDeal/ContractApplication.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * PHP Deal framework
+ *
+ * @copyright Copyright 2014, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace PhpDeal;
+
+use Go\Core\AspectContainer;
+use Go\Core\AspectKernel;
+use PhpDeal\Aspect\ContractCheckerAspect;
+
+/**
+ * Main kernel class for enabling "design by contract" paradigm
+ */
+class ContractApplication extends AspectKernel
+{
+
+    /**
+     * Configures an AspectContainer with advisors, aspects and pointcuts
+     *
+     * @param AspectContainer $container
+     *
+     * @return void
+     */
+    protected function configureAop(AspectContainer $container)
+    {
+        $container->registerAspect(new ContractCheckerAspect());
+    }
+}
