e10beee91ecd7dfa840634eb09e266ea0afa32f8
Alexander Lisachenko <lisachenko.it@gmail.com>
2014-01-30 19:08:56 +0400

    Add general annotations for pre- and post-conditions

---
+++ src/PhpDeal/Annotation/Ensure.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHP Deal framework
+ *
+ * @copyright Copyright 2014, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace PhpDeal\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * This annotation defines a Post-condition check, applied to the method
+ *
+ * @Annotation
+ * @Target("METHOD")
+ */
+class Ensure extends Annotation
+{
+
+}
