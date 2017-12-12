08885782c20b41c9556d594adf96f068b6f99a4e
Alexander Lisachenko <lisachenko.it@gmail.com>
2017-12-12 12:26:20 +0300

    Add local phpUnit and fix autoloading for test classes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,6 @@
 
 use PhpDeal\ContractApplication;
 
-if (defined("AUTOLOAD_PATH")) {
-    if (is_file(__DIR__ . '/../' .AUTOLOAD_PATH)) {
-        $loader = include_once __DIR__ . '/../' . AUTOLOAD_PATH;
-        $loader->addPsr4('PhpDeal\\', __DIR__);
-    } else {
-        throw new InvalidArgumentException("Cannot load custom autoload file located at ".AUTOLOAD_PATH);
-    }
-}
-
 if (!class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
