e0b7c6fafa20fc314c4fd708a608ce0677caaf07
Laurent Cetinsoy <laurent.cetinsoy@gmail.com>
2020-04-29 23:45:17 +0200

    easier demo setup
    put demo file in src in order to be able to have a composer.json
    in the demo folder.
    
    make contract pass and then fail at the end of demo
    to show the output

---
+++ demo/src/cache/Demo/Account.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * PHP Deal framework
+ *
+ * @copyright Copyright 2019, Lisachenko Alexander <lisachenko.it@gmail.com>
+ *
+ * This source file is subject to the license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Demo;
+
+use PhpDeal\Annotation as Contract;
+
+/**
+ * Simple trade account class
+ * @Contract\Invariant("$this->balance >= 0")
+ */
+class Account__AopProxied implements AccountContractInterface
+{
+
+    /**
+     * Current balance
+     *
+     * @var float
+     */
+    protected $balance = 0.0;
+
+    /**
+     * Deposits fixed amount of money to the account
+     *
+     * @param float $amount
+     *
+     * @Contract\Verify("$amount>0 && is_numeric($amount)")
+     * @Contract\Ensure("$this->balance == $__old->balance+$amount")
+     */
+    public function deposit($amount)
+    {
+        $this->balance += $amount;
+    }
+
+    /**
+     * @Contract\Inherit()
+     * @param float $amount
+     */
+    public function withdraw($amount)
+    {
+        $this->balance -= $amount;
+    }
+
+    /**
+     * Returns current balance
+     *
+     * @Contract\Ensure("$__result == $this->balance")
+     *
+     * @return float
+     */
+    public function getBalance()
+    {
+        return $this->balance;
+    }
+}
+include_once AOP_CACHE_DIR . '/_proxies/Demo/Account.php/Demo/Account.php';
--- demo/src/demo.php
+++ demo/src/demo.php
@@ -16,10 +16,15 @@ include_once __DIR__.'/aspect_bootstrap.php';
 
 $account = new Demo\Account();
 
-echo 'Deposit: 100' . PHP_EOL;
-$account->deposit(100);
+echo 'Deposit: 200' . PHP_EOL;
+$account->deposit(200);
 echo 'Current balance: ' . $account->getBalance();
 echo PHP_EOL;
 echo 'Withdraw: 50' . PHP_EOL;
 $account->withdraw(50);
-echo 'Current balance: ' . $account->getBalance();
+echo 'Current balance: ' . $account->getBalance(), PHP_EOL;
+
+echo "current contract allows withdrawing amount less than 50", PHP_EOL;
+
+echo "trying withdrawing 70 should fail", PHP_EOL;
+$account->withdraw(70);
